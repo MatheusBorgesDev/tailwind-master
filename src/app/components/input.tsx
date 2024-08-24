a8dbbb678e1075ca22b4d9b1ba8d3548e63a6c58
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-08-23 23:16:04 -0300

    feat: :lipstick: add sidebar dark theme

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -12,7 +12,7 @@ export function InputControl(props: InputControlProps) {
   return (
     <input
       type="text"
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:bg-transparent dark:text-zinc-100 dark:placeholder-zinc-400 dark:focus:placeholder-zinc-300"
       {...props}
     />
   );
@@ -23,7 +23,7 @@ type InputRootProps = ComponentProps<"div">;
 export function InputRoot(props: InputRootProps) {
   return (
     <div
-      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-2 focus-within:ring-violet-100"
+      className={`dark:bgzinc-800 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-2 focus-within:ring-violet-100 dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-400 dark:focus-within:ring-violet-500/10 ${props.className}`}
       {...props}
     />
   );
--- src/app/components/sidebar/nav-item.tsx
+++ src/app/components/sidebar/nav-item.tsx
@@ -10,15 +10,15 @@ export function NavItem({ icon: Icon, title }: NavItemProps) {
   return (
     <a
       href=""
-      className="group flex items-center gap-3 rounded px-3 py-2 transition hover:bg-violet-50"
+      className="group flex items-center gap-3 rounded px-3 py-2 transition hover:bg-violet-50 dark:hover:bg-zinc-800"
     >
       <Icon className="h-5 w-5 text-zinc-500" />
 
-      <span className="font-medium text-zinc-700 transition group-hover:text-violet-400">
+      <span className="font-medium text-zinc-700 transition group-hover:text-violet-400 dark:text-zinc-100 dark:group-hover:text-violet-300">
         {title}
       </span>
 
-      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 transition group-hover:text-violet-300" />
+      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 transition group-hover:text-violet-300 dark:text-zinc-600" />
     </a>
   );
 }
